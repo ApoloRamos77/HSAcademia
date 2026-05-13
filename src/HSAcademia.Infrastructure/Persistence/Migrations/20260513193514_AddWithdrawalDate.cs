3297fd2093c9ec27a6bd350b36c50ee61e9f69ac
ApoloRamos77 <aerp77@gmail.com>
2026-05-13 14:36:05 -0500

    Fix: add migration for WithdrawalDate

---
+++ src/HSAcademia.Infrastructure/Persistence/Migrations/20260513193514_AddWithdrawalDate.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace HSAcademia.Infrastructure.Persistence.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddWithdrawalDate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<DateTime>(
+                name: "WithdrawalDate",
+                table: "Students",
+                type: "timestamp with time zone",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "WithdrawalDate",
+                table: "Students");
+        }
+    }
+}
